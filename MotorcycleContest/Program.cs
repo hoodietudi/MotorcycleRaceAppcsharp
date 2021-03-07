4fb63fecf6e54b1502f640574ebdf20b0646a53c
Andrei <radu.andrei20@yahoo.com>
2021-03-07 20:15:31 +0200

    first commit

---
+++ MotorcycleContest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MotorcycleContest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
